85d9199039384599398a1b27b16aa56347cdfe12
Валерий Гуров <ValeriyGourov@live.ru>
2020-06-09 21:16:30 +0300

    Добавлен дизайн и компоненты для него

---
+++ Application/Components/Skill.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Application.Components
+{
+	public partial class Skill
+	{
+		private byte _percent;
+
+		[Parameter]
+		public byte Percent
+		{
+			get => _percent;
+			set
+			{
+				if (value > 100)
+				{
+					throw new ArgumentOutOfRangeException(nameof(value), value, "Значение процентов должно быть в диапазоне от 0 до 100.");
+				}
+				_percent = value;
+			}
+		}
+
+		[Parameter]
+		public string Title { get; set; } = null!;
+	}
+}
