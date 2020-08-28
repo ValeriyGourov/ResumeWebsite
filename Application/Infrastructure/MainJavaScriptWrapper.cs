165b74e56901653353bb088116b8db1a01733fdb
Valeriy Gourov <ValeriyGourov@live.ru>
2020-08-29 02:49:18 +0300

    Ошибка главного контейнера (#4)
    * Исправлена ошибка с постоянной видимостью сообщения об ошибке и необходимости перезагрузки страницы
    
    * Добавлены недостающие файлы для "Nivo Lightbox"
    
    * Выполнен частичный переход на JQuery 3.5.1
    
    * Изменён порядок следования стилей в элементах компонентов
    
    * Исправлена ошибка показа главного контейнера при загрузке страницы

---
+++ Application/Infrastructure/MainJavaScriptWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.JSInterop;
+
+namespace Application.Infrastructure
+{
+	/// <summary>
+	/// Обёртка для вызова функций JavaScript файла main.js.
+	/// </summary>
+	internal class MainJavaScriptWrapper
+	{
+		private readonly IJSRuntime _jsRuntime;
+
+		public MainJavaScriptWrapper(IJSRuntime jsRuntime)
+		{
+			_jsRuntime = jsRuntime ?? throw new ArgumentNullException(nameof(jsRuntime));
+		}
+
+		/// <summary>
+		/// Показывает главный контейнер приложения и скрывает вращатель.
+		/// </summary>
+		public void ShowMainContainer() => _jsRuntime.InvokeVoidAsync("showMainContainer");
+	}
+}
---
+++ Application/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using Application.Infrastructure;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Application.Shared
+{
+	public partial class MainLayout
+	{
+		/// <summary>
+		/// Обёртка для вызова функций JavaScript.
+		/// </summary>
+		[Inject] private MainJavaScriptWrapper JSWrapper { get; set; } = null!;
+
+		protected override Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				JSWrapper.ShowMainContainer();
+			}
+
+			return base.OnAfterRenderAsync(firstRender);
+		}
+	}
+}
