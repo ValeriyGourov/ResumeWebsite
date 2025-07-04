d2f5124f2fa30556f8fc307436706ebdf730f558
Валерий Гуров <Valeriy.Gourov@outlook.com>
2025-07-04 21:51:21 +0300

    Добавление возможности скачивать резюме в виде файла PDF

---
+++ Sources/Application/Services/PdfGeneration/Components/SkillProgressBarComponent.cs
@@ -0,0 +1,36 @@
+﻿using QuestPDF.Elements;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace Application.Services.PdfGeneration.Components;
+
+/// <summary>
+/// Представляет собой динамический компонент индикатора прогресса навыков, который
+/// отображает прогресс в процентах.
+/// </summary>
+/// <remarks>
+/// Этот компонент создаёт индикатор выполнения с шириной, пропорциональной указанному
+/// проценту. Прогресс-бар оформляется с использованием цветов и размеров текущей темы.
+/// </remarks>
+/// <param name="percent">Отображаемый процент прогресса, в диапазоне от 0 до 100.</param>
+internal class SkillProgressBarComponent(byte percent) : IDynamicComponent
+{
+	private readonly byte _percent = percent;
+
+	public DynamicComponentComposeResult Compose(DynamicContext context)
+	{
+		float width = context.AvailableSize.Width / 100 * _percent;
+
+		IDynamicElement content = context.CreateElement(element => _ = element
+			.Background(Theme.Colors.Border)
+			.Height(Theme.Sizes.ProgressBarHeight)
+			.Width(width)
+			.Background(Theme.Colors.Primary));
+
+		return new DynamicComponentComposeResult
+		{
+			Content = content,
+			HasMoreContent = false
+		};
+	}
+}
---
+++ Sources/Application/Services/PdfGeneration/PdfGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using QuestPDF.Companion;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace Application.Services.PdfGeneration;
+
+/// <summary>
+/// Предоставляет функциональность для создания PDF-документов с помощью библиотеки QuestPDF.
+/// </summary>
+/// <remarks>
+/// Этот класс отвечает за создание PDF-документов на основе предоставленного шаблона
+/// документа. Он поддерживает генерацию PDF в виде потока и, при необходимости, отображение
+/// документа в приложении QuestPDF Companion, если включён соответствующий параметр
+/// конфигурации.
+/// </remarks>
+/// <param name="logger">Средство ведения журнала.</param>
+/// <param name="configuration">Конфигурация приложения.</param>
+/// <param name="document">Экземпляр класса документа, формирующий файл PDF.</param>
+internal sealed partial class PdfGenerator(
+	ILogger<PdfGenerator> logger,
+	IConfiguration configuration,
+	IDocument document)
+	: IPdfGenerator
+{
+	private readonly IConfiguration _configuration = configuration;
+	private readonly IDocument _document = document;
+
+	static PdfGenerator()
+	{
+		QuestPDF.Settings.License = LicenseType.Community;
+		QuestPDF.Settings.UseEnvironmentFonts = false;
+	}
+
+	public async ValueTask<Stream> GenerateAsync()
+	{
+		if (_configuration.GetValue("PdfGeneration:ShowInCompanion", false))
+		{
+			LogInformationPdfToCompanion();
+
+			await _document
+				.ShowInCompanionAsync()
+				.ConfigureAwait(false);
+		}
+
+		MemoryStream memoryStream = new();
+
+		_document.GeneratePdf(memoryStream);
+		memoryStream.Position = 0;
+
+		return memoryStream;
+	}
+
+	#region Методы журналирования
+
+	[ExcludeFromCodeCoverage]
+	[LoggerMessage(
+		Level = LogLevel.Information,
+		Message = @"PDF будет сформирован в запущенном приложении ""QuestPDF Companion"".")]
+	private partial void LogInformationPdfToCompanion();
+
+	#endregion
+}
